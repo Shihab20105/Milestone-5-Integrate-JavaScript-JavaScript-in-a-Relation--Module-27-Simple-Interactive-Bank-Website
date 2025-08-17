1bc0b41f149530d3e346b67fd3883d090c91eb14
shihab20105 <shihabur10@gmail.com>
2025-08-17 23:46:43 +0100

    Expolred Get User deposit and set deposit total value

--- 27-6 Get User deposit and set deposit/JS/login.js
+++ 27-6 Get User deposit and set deposit/JS/login.js
@@ -5,11 +5,13 @@
 //Step 1: Add click event handler with the submit button
 document.getElementById('btn-submit').addEventListener('click', function(){
     //console.log('Submit Button Clicked ');
+
     //Step 2: Get the email address inside the email input field
     //Always remember to use '.value' to get the text from an input field
     const emailField = document.getElementById('user-email');
     const email = emailField.value;
     //console.log(email);
+
     //Step 3: Get password
     //3.a: Set id on the HTML element
     //3.b: Get the element
@@ -17,6 +19,7 @@ document.getElementById('btn-submit').addEventListener('click', function(){
     const passwordField = document.getElementById('user-password');
     const password = passwordField.value;
     //console.log(email, password);
+    
     //Danger!!: Do not verify email password on the client side
     //Step 4: Verify email and password and check whether valid user or not
     if(email === 'shontanbaap@gmail.com' && password === 'secret'){
