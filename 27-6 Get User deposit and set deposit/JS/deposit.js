c6263972f1b71322fc2bec5ba56d1aec7423e5ec
shihab20105 <shihabur10@gmail.com>
2025-08-18 13:37:56 +0100

    Explored Get User deposit and set deposit total value

--- 27-6 Get User deposit and set deposit/JS/deposit.js
+++ 27-6 Get User deposit and set deposit/JS/deposit.js
@@ -1,17 +1,26 @@
 /*                          27-6 Get User deposit and set deposit total value */
+
+/* 
+Step-1: Add event listener to the deposit button
+
+Step-2: Get the deposit amount from the deposit input field
+    2-1: For input field use '.value' to the value inside the input field
+
+Step-3: Get the current  deposit total amount
+    3-1: For non-input(element other than input, textarea) use 'innerText' to get the text / Use 'innerText' to get the text content of elements, except for <input> and <textarea>.
+*/
+
 //console.log('deposit.js');
-//Step 1: Add event listener to the deposit button
+//Step-1
 document.getElementById('btn-deposit').addEventListener('click', function(){
     //console.log('Deposit Button Clicked');
 
-    //Step 2: Get the deposit amount from the deposit input field
-    // For input field use '.value' to the value inside the input field
+    //Step-2
     const depositField = document.getElementById('deposit-field');
     const depositAmount = depositField.value;
     //console.log(depositAmount);
     
-    //Step 3: Get the current  deposit total amount
-    //For non-input(element other than input, textarea) use 'innerText' to get the text / Use 'innerText' to get the text content of elements, except for <input> and <textarea>.
+    //Step-3
     const depositTotalElement = document.getElementById('deposit-total'); 
     const depositTotal = depositTotalElement.innerText;
     //console.log(depositTotal);
